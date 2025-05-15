a4cf4ba47e3b8d071a13f95b841247d0bc9a7bcc
lbrh <s4051927@student.rmit.edu.au>
2025-05-15 15:06:16 +1000

    whoopsies forgot to actually commit the real liam file

---
+++ src/app/liam/page.tsx
@@ -0,0 +1,14 @@
+
+import Layout from "../../components/layout";
+
+export default function Home() {
+    return (
+
+        <Layout centerMain={true}>
+            <p className= " pt-50 p-10 font-sans text-8xl">WELCOME TO</p>
+
+            <p className= " pb-48 p-10 font-sans text-8xl">RMIT BATTLEBOTS</p>
+        </Layout>
+
+    );
+}
