00a83a94dcef17c267460c3711d6c06df1c3827a
lbrh <s4051927@student.rmit.edu.au>
2025-05-15 16:17:47 +1000

    fancy button implementations

---
+++ src/components/button.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { ReactElement } from "react";
+
+export default function Button (input: { Href: string; ButName: string; }): ReactElement {
+    const linkname: string = input.Href ? input.Href : "none" ;
+    const butname: string = input.ButName ? input.ButName : "none" ;
+    function logConsoleLocal(){
+
+        console.log("Oh hi mark");
+    }
+    return (
+        <a href={linkname} >
+            <button onClick={logConsoleLocal} className="bg-red-400 font-black text-gray-800 rounded-3xl px-10 py-3 hover:cursor-pointer transition-all hover:animate-pulse hover:bg-red-300">
+                {butname}
+            </button>
+        </a>
+    )
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,36 +1,21 @@
 import Link from "next/link";
+import Button from "@/components/button";
 
 const Header = () => {
 
     return (
 
         <nav className=" bg-red-700 h-25 sticky top-0" >
-            <div className="bg-white h-24 flex gap-18 justify-items-stretch items-center items drop-shadow-xl sticky top-0" >
+            <div className="bg-white h-24 flex gap-8 justify-items-stretch items-center items drop-shadow-xl sticky top-0" >
 
             <img alt={"temp"} className="object-contain rounded-md h-32 w-32" src="/Logo_Trans.png">
 
             </img>
 
-            <Link href="/about">
-                <h1 className="text-2xl font-bold text-center text-[#000000]">
-                    About Us
-                </h1>
-            </Link>
-            <Link href="/events">
-                <h1 className="text-2xl font-bold text-center text-[#000000]">
-                    Our Events
-                </h1>
-            </Link>
-            <Link href="/teams">
-                <h1 className="text-2xl font-bold text-center text-[#000000]">
-                    Our Teams
-                </h1>
-            </Link>
-            <Link href="/contact">
-                <h1 className="text-2xl font-bold text-center text-[#000000]">
-                    Contact Us
-                </h1>
-            </Link>
+            <Button Href="/about" ButName="About Us"/>
+            <Button Href="/events" ButName="Our Events"/>
+            <Button Href="/teams" ButName="Our Teams"/>
+            <Button Href="/contact" ButName="Contact Us"/>
             </div>
         </nav>
 
